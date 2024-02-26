f50d5020442b1c150d26dff726e447d1ce60136c
Adrienn <adriennlenart@yahoo.com>
2024-02-26 11:54:14 +0100

    refactor: refactor functions for Init view

--- src/views/InitView/utils.ts
+++ src/views/InitView/utils.ts
@@ -1,52 +1,37 @@
 import OpenAI from 'openai'
 import { generate } from 'random-words'
 import constants from '@/constants/constants.ts'
-import useGoodNightTaleStore from '@/stores/goodnighttale'
 
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPEN_API_KEY,
   dangerouslyAllowBrowser: true
 })
-const taleStore = useGoodNightTaleStore()
-
-export function createInitialPromptMessage(keywords: string[]) {
-  const stream: OpenAI.Chat.ChatCompletionMessageParam[] = [
-    {
-      role: 'system',
-      content: constants.chatGPTPrompt
-    },
-    {
-      role: 'user',
-      content: `The ${keywords.length} words are: ${keywords.slice(0).join(', ')}.`
-    }
-  ]
-
-  return stream
-}
 
 export const callChatGPT = async (
   messages: OpenAI.Chat.ChatCompletionMessageParam[]
 ): Promise<OpenAI.Chat.ChatCompletion | undefined> => {
-  let completion
-  try {
-    completion = await openai.chat.completions.create({ messages, model: constants.model })
-  } catch {
-    taleStore.isTaleRequestFailed = true
-  }
-  return completion
+  return openai.chat.completions.create({ messages, model: constants.chatModel })
 }
 
 export const extractPromptsForImages = (prompt: string) => {
-  const prompt1 = prompt?.split('\n')[0].split(': ')[1]
-  const prompt2 = prompt?.split('\n')[1].split(': ')[1]
-  return [prompt1, prompt2]
+  let prompts
+  if (prompt) {
+    prompts = prompt?.split('\n').map((p) => p.split(': ')[1])
+  }
+  return prompts
 }
 
-export const generateRandomWords = () => {
+export const getRandomWords = () => {
   if (localStorage.getItem(constants.randomWordsStorageKey)) {
     return JSON.parse(localStorage.getItem(constants.randomWordsStorageKey)!)
   }
   const randomWords = generate({ exactly: 10, minLength: 3 })
   localStorage.setItem(constants.randomWordsStorageKey, JSON.stringify(randomWords))
   return randomWords
+}
+
+export const generateRandomWords = () => {
+  const words = generate({ exactly: 10, minLength: 3 })
+  localStorage.setItem(constants.randomWordsStorageKey, JSON.stringify(words))
+  return words
 }
