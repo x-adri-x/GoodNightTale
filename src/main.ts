9f8505f71ee6d6d0e2de32851e0a7c83b576343b
Adrienn <adriennlenart@yahoo.com>
2024-02-26 13:04:22 +0100

    fix: remove leftover primevue config

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify'
-import PrimeVue from 'primevue/config'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
-import 'primevue/resources/themes/aura-light-green/theme.css'
 import 'vuetify/styles'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
@@ -32,6 +30,5 @@ const vuetify = createVuetify({
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
-app.use(PrimeVue)
 
 app.mount('#app')
