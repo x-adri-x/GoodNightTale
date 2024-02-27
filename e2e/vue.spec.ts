f910cdce1be1b254b093ccb249ea8fcca2f69c24
Adrienn <adriennlenart@yahoo.com>
2024-02-27 18:45:08 +0100

    test: add e2e tests

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test'
 
+test.use({ colorScheme: 'dark' })
+
 test.describe('Test Home Screen', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
@@ -7,7 +9,7 @@ test.describe('Test Home Screen', () => {
   test('visits the app root url', async ({ page }) => {
     await expect(
       page.getByRole('heading', {
-        name: 'The Good Night Tale'
+        name: 'Good Night Tale App'
       })
     ).toBeVisible()
   })
@@ -15,9 +17,7 @@ test.describe('Test Home Screen', () => {
     const button = await page.getByRole('button', { name: 'Tell me a tale!' })
     await expect(button).toBeEnabled()
     await button.click()
-    await page.getByRole('heading', {
-      name: "Let's get started!"
-    })
+    await page.getByLabel("Let's get started").waitFor()
     await expect(page.url()).toBe('http://localhost:5173/init')
   })
 })
@@ -26,35 +26,123 @@ test.describe('Test Initialize Screen', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/init')
   })
-  test('adding a keyword appears as a chip', async ({ page }) => {
-    await expect(page.getByRole('textbox')).toBeVisible()
-    await page.getByRole('textbox').fill('keyword-1')
-    await page
-      .getByRole('button', {
-        name: 'Add keyword'
-      })
-      .click()
-    await expect(page.getByTestId('keyword-1')).toBeVisible()
+  test('header is correctly rendered', async ({ page }) => {
+    const logo = await page.getByAltText('logo')
+    const heading = await page.getByLabel('Good night tale')
+    await expect(logo).toBeVisible()
+    await expect(logo).toHaveCSS('margin-right', '15px')
+    await expect(heading).toHaveText('Good Night Tale')
   })
-  test.skip('click on Generate Tale button navigates to content screen', async ({ page }) => {
-    await expect(
-      page.getByRole('button', {
-        name: 'Generate Tale'
-      })
-    ).toBeVisible()
-    await page
-      .getByRole('button', {
-        name: 'Generate Tale'
-      })
-      .click()
-    await page.waitForTimeout(5000)
-    await expect(page.url()).toBe('http://localhost:5173/content')
+  test('Init view heading is correctly rendered', async ({ page }) => {
+    const heading = await page.getByLabel("Let's get started")
+    await expect(heading).toHaveText("Let's get started!")
+    await expect(heading).toHaveCSS('margin-bottom', '40px')
+  })
+  test('info toast message is shown and rendered correctly', async ({ page }) => {
+    const infoToast = await page.locator('[data-test="info-toast"]')
+    await expect(infoToast).toBeVisible()
+    await expect(infoToast).toHaveCSS('background-color', 'rgb(33, 150, 243)')
+    await expect(infoToast).toHaveText(
+      'Please add 5 keywords of your choice or choose from the provided list.'
+    )
+  })
+  test('info toast message could be closed', async ({ page }) => {
+    const infoToast = await page.locator('[data-test="info-toast"]')
+    await expect(infoToast).toBeVisible()
+    await page.getByLabel('Close').click()
+    await expect(infoToast).not.toBeVisible()
+  })
+  test('random keywords list is shown', async ({ page }) => {
+    const container = await page.locator('[data-test="chip-container"]')
+    await expect(container).toBeVisible()
+    const chips = await container.locator('.chip').all()
+    await expect(chips).toHaveLength(10)
+    await expect(container).toHaveCSS('margin-bottom', '40px')
+    await expect(container).toHaveCSS('margin-top', '40px')
+  })
+  test('adding a keyword appears as a chip', async ({ page }) => {
+    const input = await page.getByLabel('Keyword')
+    await input.fill('foo')
+    await input.press('Enter')
+    await expect(page.locator('[data-test="selected"]').locator('.chip').first()).toHaveText('foo')
+  })
+  test('selecting from random words list is shown as a chip', async ({ page }) => {
+    const chips = await page.locator('[data-test="chip-container"]')
+    const first = await chips.locator('.chip').first()
+    const text = await first.textContent()
+    await first.click()
+    await expect(page.locator('[data-test="selected"]').locator('.chip').first()).toHaveText(
+      text as string
+    )
+  })
+  test('random words could be regenerated', async ({ page }) => {
+    const wordsBefore = await page.locator('[data-test="chip-container"]').textContent()
+    await page.locator('[data-test="regenerate-words"]').click()
+    const wordsAfter = await page.locator('[data-test="chip-container"]').textContent()
+    await expect(wordsBefore).not.toEqual(wordsAfter)
+  })
+  test('only 5 keywords can be added', async ({ page }) => {
+    const input = await page.getByLabel('Keyword')
+    await input.fill('foo')
+    await input.press('Enter')
+    await input.fill('bar')
+    await input.press('Enter')
+    await input.fill('baz')
+    await input.press('Enter')
+    await input.fill('foobar')
+    await input.press('Enter')
+    await input.fill('foobarbaz')
+    await input.press('Enter')
+    await expect(input).toBeDisabled()
+  })
+  test('selected chips can be removed from the list', async ({ page }) => {
+    const input = await page.getByLabel('Keyword')
+    await input.fill('foo')
+    await input.press('Enter')
+    const selected = await page.locator('[data-test="selected"]')
+    let chips = await selected.locator('.chip').all()
+    await expect(chips).toHaveLength(1)
+    await page.locator('[data-test="selected"]').locator('.chip').getByLabel('Close').click()
+    chips = await selected.locator('.chip').all()
+    await expect(chips).toHaveLength(0)
+  })
+  test('a warning is shown when the same word is selected', async ({ page }) => {
+    const input = await page.getByLabel('Keyword')
+    await input.fill('foo')
+    await input.press('Enter')
+    await input.fill('foo')
+    await input.press('Enter')
+    await expect(page.getByRole('alert').nth(2)).toBeVisible()
+  })
+  test('button is disabled until 5 keywords are selected', async ({ page }) => {
+    const button = await page.getByRole('button', { name: 'Generate Tale' })
+    await expect(button).toBeDisabled()
+  })
+  test('header is shown', async ({ page }) => {
+    await expect(page.locator('[data-test="header"]')).toBeVisible()
+  })
+  test('button is enabled after 5 keywords are selected', async ({ page }) => {
+    const button = await page.getByRole('button', { name: 'Generate Tale' })
+    await expect(button).toBeDisabled()
+    const input = await page.getByLabel('Keyword')
+    await input.fill('foo')
+    await input.press('Enter')
+    await input.fill('bar')
+    await input.press('Enter')
+    await input.fill('baz')
+    await input.press('Enter')
+    await input.fill('foobar')
+    await input.press('Enter')
+    await input.fill('foobarbaz')
+    await input.press('Enter')
+    await expect(button).toBeEnabled()
   })
 })
 
 test.describe('Test Content Screen', () => {
-  test.beforeEach(async ({ page }) => {
+  test('fallback message is shown when localStorage empty', async ({ page }) => {
     await page.goto('/content')
+    await expect(page.locator('[data-test="fallback-msg"]')).toHaveText('There is nothing to show.')
+    await expect(page.locator('[data-test="home-link"]')).toHaveText('Home')
   })
-  test.fixme('toast message is shown when request fails', async () => {})
 })
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,5 @@
-import process from "node:process";
-import { defineConfig, devices } from "@playwright/test";
+import process from 'node:process'
+import { defineConfig, devices } from '@playwright/test'
 
 /**
  * Read environment variables from file.
@@ -11,7 +11,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./e2e",
+  testDir: './e2e',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -19,7 +19,7 @@ export default defineConfig({
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000,
+    timeout: 5000
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
@@ -28,55 +28,49 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:5173",
+    baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: 'on-first-retry',
 
     /* Only on CI systems run the tests headless */
-    headless: !!process.env.CI,
+    headless: !!process.env.CI
   },
 
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: "chromium",
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
-    // {
-    //   name: 'firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox']
-    //   }
-    // },
-    // {
-    //   name: 'webkit',
-    //   use: {
-    //     ...devices['Desktop Safari']
-    //   }
-    // }
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome']
+      }
+    },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari']
+      }
+    },
 
     /* Test against mobile viewports. */
     {
-      name: "Mobile Chrome",
+      name: 'Mobile Chrome',
       use: {
-        ...devices["Pixel 5"],
-      },
+        ...devices['Pixel 5']
+      }
     },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
+    {
+      name: 'Mobile Safari',
+      use: {
+        ...devices['iPhone 12']
+      }
+    }
 
     /* Test against branded browsers. */
     // {
@@ -103,8 +97,8 @@ export default defineConfig({
      * Use the preview server on CI for more realistic testing.
      * Playwright will re-use the local server if there is already a dev-server running.
      */
-    command: process.env.CI ? "vite preview --port 5173" : "vite dev",
+    command: process.env.CI ? 'vite preview --port 5173' : 'vite dev',
     port: 5173,
-    reuseExistingServer: !process.env.CI,
-  },
-});
+    reuseExistingServer: !process.env.CI
+  }
+})
